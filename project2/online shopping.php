20ed222b795185fb7f1884b789d41aa4a37260ec
yaswanth <113416864+yaswanth7842@users.noreply.github.com>
2023-11-24 21:27:26 +0530

    Create online shopping.php

---
+++ project2/online shopping.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Online Store</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+
+        nav {
+            background-color: #444;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+
+        nav a {
+            color: #fff;
+            text-decoration: none;
+            padding: 10px 20px;
+            display: inline-block;
+        }
+
+        nav a:hover {
+            background-color: #555;
+        }
+
+        .search-bar {
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        #search-input {
+            padding: 10px;
+            width: 300px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .call-to-action {
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        .call-to-action a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .call-to-action a:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Your Online Store</h1>
+    </header>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="shoppingcart.php">Products</a>
+        <a href="#">Categories</a>
+        <a href="#">Contact</a>
+    </nav>
+
+    <div class="search-bar">
+        <input type="text" id="search-input" placeholder="Search for products...">
+    </div>
+
+    <div class="call-to-action">
+        <a href="logins.php">Shop Now</a>
+    </div>
+    
+
+    <!-- Add your product listings, featured products, or any other content here -->
+
+</body>
+</html>
